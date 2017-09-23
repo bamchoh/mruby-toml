a1cc04f762be9a305cc7e5df34532c7810dfd670
bamchoh <bamchoh@gmail.com>
2017-09-23 12:22:19 +0900

    Add paths for libtoml

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -3,4 +3,12 @@ MRuby::Build.new do |conf|
   conf.gembox 'default'
   conf.gem '../mruby-toml'
   conf.enable_test
+  
+  conf.cc do |cc|
+    cc.include_paths << "../libtoml"
+  end
+  
+  conf.linker do |linker|
+    linker.library_paths << "../libtoml"
+  end
 end
