c2443ebef04d84503ce594d3c5e44f5b3f6c8f04
bamchoh <bamchoh@gmail.com>
2017-10-02 21:10:00 +0900

    Update mrb_toml.rb

--- test/mrb_toml.rb
+++ test/mrb_toml.rb
@@ -3,22 +3,26 @@
 ##
 
 assert("Toml#parse(root of hash") do
+  toml = "bar = 1"
   exp = {"bar" => 1}
-  assert_equal(exp, Toml.parse("bar = 1"))
+  assert_equal(exp, Toml.parse(toml))
 end
 
 assert("Toml#parse(table)") do
+  toml = "[foo]\nbar = 1\n"
   exp = {"foo" => {"bar" => 1}}
-  assert_equal(exp, Toml.parse("[foo]\nbar = 1\n"))
+  assert_equal(exp, Toml.parse(toml))
 end
 
 assert("Toml#parse(array)") do
+  toml = "bar = [1,2,3]"
   exp = {"bar" => [1,2,3]}
-  assert_equal(exp, Toml.parse("bar = [1,2,3]"))
+  assert_equal(exp, Toml.parse(toml))
 end
 
 assert("Toml#parse(array of tables)") do
+  toml = "[[foo]]\nbar = 1\nfiz = 2\n\n[[foo]]\nabc = 3\ndef = 4\n"
   exp = {"foo" => [{"bar" => 1, "fiz" => 2}, {"abc" => 3, "def" => 4}]}
-  assert_equal(exp, Toml.parse("[[foo]]\nbar = 1\nfiz = 2\n\n[[foo]]\nabc = 3\ndef = 4\n"))
+  assert_equal(exp, Toml.parse(toml))
 end
 
